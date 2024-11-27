a2e4c32331a2ba87968249f04d8d557624310219
Shivkumar Kothale <skothale@cs.binghamton.edu>
2024-11-27 18:25:07 -0500

    updated

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -5,20 +5,31 @@
 
 parse(X) :- lines(X, []).
 
-lines([], []).
-lines(X, A) :- line(X, Y), colonOp(Y, Z), lines(Z, A).
-lines(X, Y) :- line(X, Y).
+% Rules for parsing lines
+lines([], []). % Base case: no input, no output.
+lines(X, Remainder) :- line(X, Temp), colonOp(Temp, Temp2), lines(Temp2, Remainder).
+lines(X, Remainder) :- line(X, Remainder). % Alternative case: single line.
 
-line(X, A) :- num(X, Y), commaOp(Y, Z), line(Z, A).
-line(X, Y) :- num(X, Y).
+% Rules for parsing a line
+line(X, Remainder) :- nums(X, Temp), commaOp(Temp, Temp2), line(Temp2, Remainder).
+line(X, Remainder) :- nums(X, Remainder). % Single number case.
 
-num(X, Y) :- digit(X, Y).
-num(X, Z) :- digit(X, Y), num(Y, Z).
+% Rule for parsing numbers
+nums([Digit | Rest], Remainder) :-
+    digit(Digit),
+    number_tail(Rest, Remainder).
 
+number_tail([Digit | Rest], Remainder) :-
+    digit(Digit), 
+    number_tail(Rest, Remainder).
+number_tail(Remainder, Remainder). % Base case: no more digits.
+
+% Operators
 colonOp([';' | T], T).
 commaOp([',' | T], T).
 
-digit([X | T], T) :-
+% Rule for single-digit validation
+digit(X) :-
     member(X, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']).
 
 % Example execution:
--- search/search.pl
+++ search/search.pl
@@ -2,49 +2,57 @@
 % Your code goes here:
 %%%%%%%%%%%%%%%%%%%%%%
 
-
 % Define the initial state
-initial_state(state(Position, no_red_key, no_blue_key, no_black_key)) :- 
+initial_state(state(Position, no_red_key, no_blue_key, no_black_key)) :-
     initial(Position).
 
-% Generic move action for open doors
-move_action(state(From, RedKey, BlueKey, BlackKey), To, state(To, RedKey, BlueKey, BlackKey)) :- 
-    (door(From, To); door(To, From)).
+% Define generic movement
+take_action(state(From, RedKey, BlueKey, BlackKey), move(From, To), state(To, RedKey, BlueKey, BlackKey)) :-
+    door(From, To);
+    door(To, From).
 
-% Move and pick up keys
-pick_key(state(From, no_red_key, BlueKey, BlackKey), To, red, state(To, has_red_key, BlueKey, BlackKey)).
-pick_key(state(From, RedKey, no_blue_key, BlackKey), To, blue, state(To, RedKey, has_blue_key, BlackKey)).
-pick_key(state(From, RedKey, BlueKey, no_black_key), To, black, state(To, RedKey, BlueKey, has_black_key)).
+% Define key collection actions
+take_action(state(From, no_red_key, BlueKey, BlackKey), move(From, To), state(To, has_red_key, BlueKey, BlackKey)) :-
+    door(From, To),
+    key(To, red).
 
-% Move through locked doors if the corresponding key is available
-unlock_door(state(From, has_red_key, BlueKey, BlackKey), To, red, state(To, has_red_key, BlueKey, BlackKey)).
-unlock_door(state(From, RedKey, has_blue_key, BlackKey), To, blue, state(To, RedKey, has_blue_key, BlackKey)).
-unlock_door(state(From, RedKey, BlueKey, has_black_key), To, black, state(To, RedKey, BlueKey, has_black_key)).
+take_action(state(From, RedKey, no_blue_key, BlackKey), move(From, To), state(To, RedKey, has_blue_key, BlackKey)) :-
+    door(From, To),
+    key(To, blue).
 
-% Take an action: move, pick up a key, or unlock a door
-take_action(CurrentState, move(From, To), NextState) :-
-    CurrentState = state(From, RedKey, BlueKey, BlackKey),
-    (   move_action(CurrentState, To, NextState)
-    ;   key(To, KeyColor), pick_key(CurrentState, To, KeyColor, NextState)
-    ;   locked_door(From, To, KeyColor), unlock_door(CurrentState, To, KeyColor, NextState)
-    ;   locked_door(To, From, KeyColor), unlock_door(CurrentState, To, KeyColor, NextState)
-    ).
+take_action(state(From, RedKey, BlueKey, no_black_key), move(From, To), state(To, RedKey, BlueKey, has_black_key)) :-
+    door(From, To),
+    key(To, black).
 
-% Define the final state as reaching the treasure
-final_state(state(Position, _, _, _)) :- 
+% Define locked door actions
+take_action(state(From, RedKey, BlueKey, BlackKey), move(From, To), state(To, RedKey, BlueKey, BlackKey)) :-
+    (locked_door(From, To, blue); locked_door(To, From, blue)),
+    BlueKey = has_blue_key.
+
+take_action(state(From, RedKey, BlueKey, BlackKey), move(From, To), state(To, RedKey, BlueKey, BlackKey)) :-
+    (locked_door(From, To, red); locked_door(To, From, red)),
+    RedKey = has_red_key.
+
+take_action(state(From, RedKey, BlueKey, BlackKey), move(From, To), state(To, RedKey, BlueKey, BlackKey)) :-
+    (locked_door(From, To, black); locked_door(To, From, black)),
+    BlackKey = has_black_key.
+
+% Define final state
+final_state(state(Position, _, _, _)) :-
     treasure(Position).
 
-% Recursive steps to transition from state to state
-take_steps(State, [Action], FinalState) :- 
+% Define steps recursively
+take_steps(State, [Action], FinalState) :-
     take_action(State, Action, FinalState).
 
 take_steps(State, [Action | Rest], FinalState) :-
     take_action(State, Action, IntermediateState),
     take_steps(IntermediateState, Rest, FinalState).
 
-% Main search logic
+
 search(Actions) :-
     initial_state(InitialState),
     length(Actions, _),
     take_steps(InitialState, Actions, FinalState),
     final_state(FinalState), !.
+
